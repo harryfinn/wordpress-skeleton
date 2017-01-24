d4f25ded62dd57a03533e1873a74995249db3312
Katie Welton <katie.welton@gmail.com>
2017-01-24 15:35:42 +0000

    Swap out comments in webfont loader for real example

--- app/javascripts/web-font-loader.js
+++ app/javascripts/web-font-loader.js
@@ -1,10 +1,7 @@
 // eslint-disable-next-line
 WebFontConfig = {
-  typekit: {
-    id: ''
-  },
   google: {
-    families: []
+    families: ['Droid Sans', 'Droid Serif']
   },
   timeout: 2000,
   active: function() { clearFout(); },
@@ -15,13 +12,10 @@ function clearFout() {
   sessionStorage.fonts = true;
 }
 
-// Uncomment the lines below and remove line 'clearFout()' once webfonts have been included.
-
 (function(d) {
-  // var wf = d.createElement('script'), s = d.scripts[0];
-  // wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
-  // s.parentNode.insertBefore(wf, s);
-  clearFout()
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+  s.parentNode.insertBefore(wf, s);
   if(sessionStorage.fonts) {
     d.getElementsByTagName('html')[0].classList.add('wf-active');
   }
